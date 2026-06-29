c6db371b84f2b6429f0b3ce0ec5a7f00d0cbffb1
dreadlordgamingyt-pixel <dreadlordgamingyt@gmail.com>
2026-06-29 10:09:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,35 @@
-// Countdown Timer
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const targetDate = new Date("July 5, 2026 20:00:00").getTime();
-
-setInterval(() => {
-
-const now = new Date().getTime();
-
-const distance = targetDate - now;
-
-const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-document.getElementById("timer").innerHTML =
-days + "d : " +
-hours + "h : " +
-minutes + "m : " +
-seconds + "s";
-
-},1000);
-
-// Live Slots Counter
-
-let slots = 0;
-const maxSlots = 12;
-
-document.getElementById("slots-count").innerHTML =
-slots + " / " + maxSlots;
-function toggleMenu(){
-
-document.querySelector("nav").classList.toggle("active");
-
-}
-const cards=document.querySelectorAll(".card,.feature,.prize-card,.slots-card");
-
-const observer=new IntersectionObserver(entries=>{
-
-entries.forEach(entry=>{
-
-if(entry.isIntersecting){
-
-entry.target.style.opacity="1";
-entry.target.style.transform="translateY(0)";
-
-}
-
-});
-
-});
-
-cards.forEach(card=>{
-
-card.style.opacity="0";
-card.style.transform="translateY(40px)";
-card.style.transition=".6s";
-
-observer.observe(card);
-
-});
 const form = document.querySelector("form");
 const success = document.getElementById("success");
+const slots = document.getElementById("slots-count");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
+async function loadSlots() {
+  const snapshot = await getDocs(collection(db, "registrations"));
+  slots.innerText = `${snapshot.size} / 12`;
+}
 
-    success.style.display = "block";
+loadSlots();
 
-    form.reset();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    setTimeout(()=>{
-        success.style.display = "none";
-    },3000);
+  const data = {
+    team: form[0].value,
+    leader: form[1].value,
+    uid: form[2].value,
+    whatsapp: form[3].value,
+    createdAt: new Date()
+  };
+
+  await addDoc(collection(db, "registrations"), data);
+
+  success.style.display = "block";
+  form.reset();
+  loadSlots();
 });
