43ed56d3aab8ab4520fed089926dc3510635180b
dreadlordgamingyt-pixel <dreadlordgamingyt@gmail.com>
2026-06-29 08:37:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+// Countdown Timer
+
 const targetDate = new Date("July 5, 2026 20:00:00").getTime();
 
 setInterval(() => {
 
 const now = new Date().getTime();
+
 const distance = targetDate - now;
 
-const days = Math.floor(distance / (1000*60*60*24));
-const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-const seconds = Math.floor((distance % (1000*60)) / 1000);
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 document.getElementById("timer").innerHTML =
-`${days}D ${hours}H ${minutes}M ${seconds}S`;
+days + "d : " +
+hours + "h : " +
+minutes + "m : " +
+seconds + "s";
 
 },1000);
-// LIVE SLOTS
+
+// Live Slots Counter
 
 let slots = 0;
 const maxSlots = 12;
 
 document.getElementById("slots-count").innerHTML =
-`${slots} / ${maxSlots}`;
+slots + " / " + maxSlots;
